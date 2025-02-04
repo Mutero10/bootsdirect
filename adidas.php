d9316553cdadf030ce1bc56a16dc6ad900b9b02d
Mutero10 <mutero.27.andrew@gmail.com>
2025-02-04 09:47:43 +0300

    Created the adidas file and added a horizontal line

---
+++ adidas.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title> Adidas</title>
+</head>
+<body>
+
+<h1 style ="text-align:center"> BEST OF ADIDAS </h1> <hr>
+    
+</body>
+</html>
