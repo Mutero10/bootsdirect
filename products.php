c4aade8e06c5681a1f37e7c1f3760181207d7bc7
Mutero10 <mutero.27.andrew@gmail.com>
2025-02-14 23:31:44 +0300

    Updated products.php to support fetching data from both 'products' and puma 'tables'

--- products.php
+++ products.php
@@ -3,6 +3,8 @@ include 'databasehandler.php';
 
 $db = new DatabaseHandler();
 $products = $db->getProducts();
+$pumaProducts = $db->getPumaProducts(); 
+
 
 if (!empty($products)) {
     foreach ($products as $row) {
@@ -21,4 +23,23 @@ if (!empty($products)) {
 } else {
     echo "<p>No products found.</p>";
 }
+
+if (!empty($pumaProducts)) {
+    foreach ($pumaProducts as $row) {
+        echo '
+        <div class="col-md-4 mb-4 product-card" data-type="' . htmlspecialchars($row["category"]) . '" data-price="' . htmlspecialchars($row["price"]) . '">
+            <div class="card h-100">
+                <img src="' . htmlspecialchars($row["image"]) . '" class="card-img-top product-image" alt="' . htmlspecialchars($row["name"]) . '">
+                <div class="card-body">
+                    <h5 class="card-title">' . htmlspecialchars($row["name"]) . '</h5>
+                    <p class="card-text">Ksh ' . htmlspecialchars($row["price"]) . '</p>
+                    <a href="#" class="btn btn-primary">View Details</a>
+                </div>
+            </div>
+        </div>';
+    }
+} else {
+    echo "<p>No puma products found.</p>";
+}
+
 ?>
