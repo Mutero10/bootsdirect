050ee9c9b111d5efd4b23802df82c3ceb519a038
Mutero10 <mutero.27.andrew@gmail.com>
2025-02-14 21:35:19 +0300

    Modified the products file to connect to the main database file

--- products.php
+++ products.php
@@ -1,25 +1,24 @@
 <?php
-$connection = new mysqli("localhost", "root", "", "mydatabase");
+include 'databasehandler.php';
 
-if ($connection->connect_error) {
-    die("Connection failed: " . $connection->connect_error);
-}
+$db = new DatabaseHandler();
+$products = $db->getProducts();
 
-$query = "SELECT * FROM products";
-$result = $connection->query($query);
-?>
-
-<div class="row">
-    <?php while ($row = $result->fetch_assoc()): ?>
-        <div class="col-md-4 product-card" data-type="<?= $row['type']; ?>" data-price="<?= $row['price']; ?>">
+if (!empty($products)) {
+    foreach ($products as $row) {
+        echo '
+        <div class="col-md-4 mb-4 product-card" data-type="' . htmlspecialchars($row["type"]) . '" data-price="' . htmlspecialchars($row["price"]) . '">
             <div class="card h-100">
-                <img src="<?= $row['image']; ?>" class="card-img-top product-image" alt="<?= $row['name']; ?>">
+                <img src="' . htmlspecialchars($row["image"]) . '" class="card-img-top product-image" alt="' . htmlspecialchars($row["name"]) . '">
                 <div class="card-body">
-                    <h5 class="card-title"><?= $row['name']; ?></h5>
-                    <p class="card-text">Ksh <?= number_format($row['price']); ?></p>
+                    <h5 class="card-title">' . htmlspecialchars($row["name"]) . '</h5>
+                    <p class="card-text">Ksh ' . htmlspecialchars($row["price"]) . '</p>
                     <a href="#" class="btn btn-primary">View Details</a>
                 </div>
             </div>
-        </div>
-    <?php endwhile; ?>
-</div>
+        </div>';
+    }
+} else {
+    echo "<p>No products found.</p>";
+}
+?>
