f26876b9afa26d46514ff311a5efe44a0db11d53
Mutero10 <mutero.27.andrew@gmail.com>
2025-02-15 23:59:01 +0300

    Created a file that displays products added to the cart

--- cart.php
+++ cart.php
@@ -1,32 +1,40 @@
 <?php
 session_start();
-include 'databasehandler.php';
-
-$data = json_decode(file_get_contents('php://input'), true);
-$productId = $data['id'];
-
-$db = new DatabaseHandler();
-$product = $db->getProductById($productId);
-
-if ($product) {
-    if (!isset($_SESSION['cart'])) {
-        $_SESSION['cart'] = [];
-    }
-
-    if (isset($_SESSION['cart'][$productId])) {
-        $_SESSION['cart'][$productId]['quantity'] += 1;
-    } else {
-        $_SESSION['cart'][$productId] = [
-            'id' => $product['id'],
-            'name' => $product['name'],
-            'price' => $product['price'],
-            'image' => $product['image'],
-            'quantity' => 1,
-        ];
-    }
-
-    echo json_encode(['success' => true]);
-} else {
-    echo json_encode(['success' => false]);
-}
 ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Shopping Cart</title>
+    <!-- Include your CSS and JS files here -->
+</head>
+<body>
+    <h1>Your Shopping Cart</h1>
+    <?php if (isset($_SESSION['cart']) && !empty($_SESSION['cart'])): ?>
+        <table>
+            <thead>
+                <tr>
+                    <th>Product</th>
+                    <th>Price</th>
+                    <th>Quantity</th>
+                    <th>Total</th>
+                </tr>
+            </thead>
+            <tbody>
+                <?php foreach ($_SESSION['cart'] as $item): ?>
+                    <tr>
+                        <td><?php echo htmlspecialchars($item['name']); ?></td>
+                        <td>Ksh <?php echo htmlspecialchars($item['price']); ?></td>
+                        <td><?php echo htmlspecialchars($item['quantity']); ?></td>
+                        <td>Ksh <?php echo htmlspecialchars($item['price'] * $item['quantity']); ?></td>
+                    </tr>
+                <?php endforeach; ?>
+            </tbody>
+        </table>
+        <a href="checkout.php" class="btn btn-success">Proceed to Checkout</a>
+    <?php else: ?>
+        <p>Your cart is empty.</p>
+    <?php endif; ?>
+</body>
+</html>
